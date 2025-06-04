4991b0d4f0e2d701f43e95b4d57c7488c89f6d92
Bruno Gonçalves <bpvgoncalves@users.noreply.github.com>
2025-06-05 00:36:41 +0200

    docs: add function documentation

--- R/key.R
+++ R/key.R
@@ -1,4 +1,24 @@
-apikey_store <- function(key_name, key, password = NULL) {
+#' API Key - Store
+#'
+#' Stores the API key securely for latter retrieval.
+#' By default it will not overwrite a previously existing key unless explicitly set.
+#'
+#' @param key_name  String. The key name.
+#' @param key       String. The API key.
+#' @param password  String. A password to encrypt the key for storage.
+#' @param overwrite Boolean. What to do if a previous key file is detected and function is called in
+#'                  non-interactive session.
+#'
+#' @returns Silently, the key file path if successful.
+#' @export
+#'
+#' @examples
+#'
+#' apikey_store("my_key_name",
+#'              "my_secret_api_key",
+#'              "a_very_strong_password_that_nobody_will_ever_find")
+#'
+apikey_store <- function(key_name, key, password = NULL, overwrite = FALSE) {
 
   key_obj <- serialize(list(name = key_name,
                             key = key),
