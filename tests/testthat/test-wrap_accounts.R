aa38e5a6089a90f6f298fe8a8f76fcda279148c9
Bruno Gonçalves <bpvgoncalves@users.noreply.github.com>
2025-06-25 22:20:13 +0200

    test: add new tests and improve existing ones
    Existing checks now check for explicit error message.

--- tests/testthat/test-wrap_accounts.R
+++ tests/testthat/test-wrap_accounts.R
@@ -10,6 +10,16 @@ test_that("'account_list' wrapper works", {
   expect_equal(accs[1, 3], "USDC")
   expect_equal(accs[1, 13], "100")
 
+  accs2 <- account_list(limit = 2L, use_sandbox = TRUE)
+  expect_identical(accs, accs2)
+
+  expect_message(accs3 <- account_list(limit = 2000L, use_sandbox = TRUE),
+                 "exceeds maximum of 250")
+  expect_identical(accs, accs3)
+
+  accs4 <- account_list(cursor = "abc", use_sandbox = TRUE)
+  expect_identical(accs, accs4)
+
 })
 
 
@@ -17,12 +27,18 @@ test_that("'account_list' throws error on bad parameters", {
 
   skip_if_offline()
 
-  expect_error(account_list(limit = NA, use_sandbox = TRUE))
-  expect_error(account_list(limit = "abc", use_sandbox = TRUE))
-  expect_error(account_list(limit = c(1, 2), use_sandbox = TRUE))
+  expect_error(account_list(limit = NA, use_sandbox = TRUE), "Invalid parameter `limit`")
+  expect_error(account_list(limit = "abc", use_sandbox = TRUE), "Invalid parameter `limit`")
+  expect_error(account_list(limit = c(1, 2), use_sandbox = TRUE), "Invalid parameter `limit`")
 
-  expect_error(account_list(cursor = NA, use_sandbox = TRUE))
-  expect_error(account_list(cursor = 123, use_sandbox = TRUE))
-  expect_error(account_list(cursor = c("a", "b"), use_sandbox = TRUE))
+  expect_error(account_list(cursor = NA, use_sandbox = TRUE), "Invalid parameter `cursor`")
+  expect_error(account_list(cursor = 123, use_sandbox = TRUE), "Invalid parameter `cursor`")
+  expect_error(account_list(cursor = c("a", "b"), use_sandbox = TRUE), "Invalid parameter `cursor`")
+
+  expect_error(account_list(use_sandbox = NULL), "Invalid parameter `use_sandbox`")
+  expect_error(account_list(use_sandbox = NA), "Invalid parameter `use_sandbox`")
+  expect_error(account_list(use_sandbox = 123), "Invalid parameter `use_sandbox`")
+  expect_error(account_list(use_sandbox = "abc"), "Invalid parameter `use_sandbox`")
+  expect_error(account_list(use_sandbox = c(TRUE, TRUE)), "Invalid parameter `use_sandbox`")
 
 })
