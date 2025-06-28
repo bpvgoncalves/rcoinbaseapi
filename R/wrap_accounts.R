62ac5a6441893069e7416ef1e43bd3ae15de865c
Bruno Gonçalves <bpvgoncalves@users.noreply.github.com>
2025-06-28 03:05:31 +0200

    docs: improve documentation

--- R/wrap_accounts.R
+++ R/wrap_accounts.R
@@ -2,9 +2,16 @@
 #' Coinbase API - Accounts - List Accounts
 #'
 #'
-#' List Coinbase Brokerage Accounts: Retrieves a list of brokerage accounts associated with the
-#' authenticated Coinbase user.
+#' This function queries the `/api/v3/brokerage/accounts` Coinbase API endpoint.
+#'
+#'
+#' Requires authentication unless `use_sandbox = TRUE`.
+#'
+#' Retrieves a list of brokerage accounts associated with the authenticated Coinbase user.
 #' Automatically handles pagination and returns a combined data frame.
+#'
+#' Input validation ensures that malformed or malicious values are rejected early. Use of
+#' this function in production environments should follow best practices for API key management.
 #'
 #' @param limit       Optional. Integer. Maximum number of results per page (1–250). If not
 #'                    provided, API default applies. Values above 250 will be capped.
@@ -12,15 +19,8 @@
 #'                    previous call).
 #' @param use_sandbox Boolean. If TRUE, uses Coinbase sandbox environment. Default: FALSE.
 #'
-#' @return A data frame containing account details. The result includes all pages if pagination is required.
-#'
-#' @details
-#' This function wraps the `/api/v3/brokerage/accounts` Coinbase API endpoint.
-#' Requires authentication unless `use_sandbox = TRUE`.
-#'
-#' Input validation ensures that malformed or malicious values are rejected early. Use of
-#' this function in production environments should follow best practices for API key management.
-#'
+#' @return A data frame containing account details. The result includes all pages if pagination is
+#' required.
 #'
 #' @seealso [apirequest()]
 #'
@@ -63,27 +63,21 @@ account_list <- function(limit = NULL, cursor = NULL, use_sandbox = FALSE) {
     cli::cli_abort("Invalid parameter `use_sandbox`: {use_sandbox}")
   }
 
-  resp <- apirequest("GET",
-                     "api/v3/brokerage/accounts",
-                     query_params = q_par,
-                     need_auth = !use_sandbox,
-                     use_sandbox = use_sandbox)
-
-  resp_data <- httr2::resp_body_json(resp, simplifyVector = TRUE, flatten = TRUE)
-
-  ret <- resp_data$accounts
-
-  while (resp_data$has_next) {
+  ret <- data.frame()
+  send_request <- TRUE
+  while (send_request) {
 
     resp <- apirequest("GET",
                        "api/v3/brokerage/accounts",
-                       query_params = list(cursor = resp_data$cursor),
+                       query_params = q_par,
                        need_auth = !use_sandbox,
                        use_sandbox = use_sandbox)
 
     resp_data <- httr2::resp_body_json(resp, simplifyVector = TRUE, flatten = TRUE)
 
     ret <- rbind(ret, resp_data$accounts)
+    send_request <- resp_data$has_next
+    q_par$cursor <- resp_data$cursor
   }
 
   invisible(ret)
